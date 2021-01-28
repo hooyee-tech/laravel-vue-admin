11e6cc51ca4de8c718bf199a74da75f6f4289bad
回一科技 <73012330+hooyee-tech@users.noreply.github.com>
2021-01-28 18:54:20 +0800

    Update DateTimePicker.php
    添加日期时间选择器的默认时间

--- src/Components/Form/DateTimePicker.php
+++ src/Components/Form/DateTimePicker.php
@@ -10,12 +10,24 @@ class DateTimePicker extends DatePicker
 
     protected $format = "yyyy-MM-dd HH:mm:ss";
     protected $valueFormat = "yyyy-MM-dd HH:mm:ss";
+    protected $defaultval = [];
 
     static public function make($value = null, $type = "datetime")
     {
         $value = $value?$value:now()->toDateTimeString();
 
         return (new DateTimePicker($value))->type($type);
+    }
+    
+    /**
+     * 可选，选择器打开时默认显示的时间
+     * @param Date $defaultValue
+     * @return $this
+     */
+    public function defaultval($defaultval)
+    {
+        $this->defaultval = $defaultval;
+        return $this;
     }
 
 }
